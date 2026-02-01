c3b94662d2035750eb0191c2aaf91e7fa04ed014
Harsh Agarwal <harshagr0110@gmail.com>
2026-02-02 02:34:10 +0530

    Remove broken clerk.d.ts type definitions

--- clerk.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@clerk/nextjs/server' {
-  export function auth(): Promise<any>
-  export function clerkMiddleware(handler: (auth: any, req: any) => Promise<void>): any
-  export function createRouteMatcher(matchers: string[]): (req: any) => boolean
-}
